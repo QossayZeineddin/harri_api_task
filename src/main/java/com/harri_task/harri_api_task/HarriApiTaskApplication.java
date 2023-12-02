8be8474c5f46a59587929f45f6f443e5358fc26d
QossayZeineddin <qossay.zeineddin@gmai.com>
2023-12-02 12:57:52 +0200

    Initial commit

---
+++ src/main/java/com/harri_task/harri_api_task/HarriApiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.harri_task.harri_api_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarriApiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarriApiTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harri_task/harri_api_task/HarriApiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harri_task.harri_api_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarriApiTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
